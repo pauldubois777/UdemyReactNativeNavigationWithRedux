1d697a5bb26d643dfad6ee904595e77b3b0c733d
Paul DuBois <pauldubois777@yahoo.com>
2020-03-29 19:34:27 -0500

    Navigation buttons

--- src/screens/CategoriesScreen.js
+++ src/screens/CategoriesScreen.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 import commonStyles from '../constants/commonStyles';
 
 const CategoriesScreen = props => {
+  const {
+    navigation: { navigate }
+  } = props;
+
   return (
     <View style={styles.screen}>
       <Text>Categories Screen</Text>
+      <Button
+        title="Go to Meals"
+        onPress={() => {
+          navigate({ routeName: 'CategoryMealsScreen' });
+        }}
+      />
     </View>
   );
 };
--- src/screens/CategoryMealsScreen.js
+++ src/screens/CategoryMealsScreen.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 import commonStyles from '../constants/commonStyles';
 
 const CategoryMealsScreen = props => {
+  const {
+    navigation: { navigate }
+  } = props;
+
   return (
     <View style={styles.screen}>
       <Text>CategoryMeals Screen</Text>
+      <Button
+        title="Meal Detail"
+        onPress={() => {
+          navigate({ routeName: 'MealDetailScreen' });
+        }}
+      />
     </View>
   );
 };
