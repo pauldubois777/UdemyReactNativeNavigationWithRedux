8d9adff2f870ecbe6785cedf116367d0a13e1976
Paul DuBois <pauldubois777@yahoo.com>
2020-03-31 19:03:49 -0500

    Styling the category item

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -1,23 +1,43 @@
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import fonts from '../constants/fonts';
 
 const CategoryItem = props => {
-  const { item } = props;
+  const { item, onPress } = props;
 
   return (
-    <TouchableOpacity onPress={props.onPress} style={styles.categoryItemView}>
-      <View>
-        <Text>{item.title}</Text>
+    <TouchableOpacity onPress={onPress} style={styles.categoryItemWrapper}>
+      <View style={{ ...styles.categoryItemView, backgroundColor: item.color }}>
+        <Text style={styles.categoryItemText} numberOfLines={2}>
+          {item.title}
+        </Text>
       </View>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-  categoryItemView: {
+  categoryItemWrapper: {
     flex: 1,
     margin: 15,
     height: 150
+  },
+  categoryItemView: {
+    flex: 1,
+    borderRadius: 10,
+    shadowColor: 'black',
+    shadowOpacity: 0.6,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 10,
+    elevation: 3,
+    padding: 10,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end'
+  },
+  categoryItemText: {
+    fontFamily: fonts.bold,
+    fontSize: 22,
+    textAlign: 'right'
   }
 });
 
